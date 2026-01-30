100120d56c84bf482805bf1207dfe3dbc4a43463
LightShi233 <lightshi233@outlook.com>
2026-01-30 18:30:05 +0800

    更新 build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -7,13 +7,13 @@ source build/envsetup.sh
 bash hybris-patches/apply-patches.sh --mb
 export USE_CCACHE=1
 breakfast $DEVICE
-#make -j$(nproc) mkbootimg
-#make -j$(nproc) fec
-#make -j$(nproc) halium-boot
+make -j$(nproc) mkbootimg
+make -j$(nproc) fec
+make -j$(nproc) halium-boot
 make -j$(nproc) systemimage 
-#make -j$(nproc) vendorimage
+make -j$(nproc) vendorimage
 
-echo "md5sum halium-boot.img system.img vendor.img"
-#md5sum $ANDROID_ROOT/out/target/product/clover/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/clover/system.img
-#md5sum $ANDROID_ROOT/out/target/product/clover/vendor.img
+#echo "md5sum halium-boot.img system.img vendor.img"
+#md5sum $ANDROID_ROOT/out/target/product/cannon/halium-boot.img
+#md5sum $ANDROID_ROOT/out/target/product/cannon/system.img
+#md5sum $ANDROID_ROOT/out/target/product/cannon/vendor.img
